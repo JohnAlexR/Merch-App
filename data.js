cbec7f151b3dcd298d4672c56e94f8ef1352fda9
JohnAlexR <128446488+JohnAlexR@users.noreply.github.com>
2023-06-07 13:50:04 -0400

    Add html and css skeleton and dummy data

---
+++ data.js
@@ -0,0 +1,60 @@
+export const inventory = [
+    {
+        item: 'Shirt',
+        color: 'black',
+        size: 's',
+        qty: 5,
+        price: 15,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+        discount: 0,
+        img: '/Images/black-demo-shirt.png',
+    },
+
+    {
+        item: 'Shirt',
+        color: 'black',
+        size: 'm',
+        qty: 10,
+        price: 15,
+        uuid: 'aa161ffe-d0f5-9545-8c4f-8462945323dd',
+        discount: 0,
+        img: '/Images/black-demo-shirt.png',
+    },
+
+    {
+        item: 'Shirt',
+        color: 'blue',
+        size: 's',
+        qty: 10,
+        price: 20,
+        uuid: 'xr161ffe-d0f5-9545-8c4f-1547945323dd',
+        discount: 0,
+        img: '/Images/blue-demo-shirt.png',
+    },
+
+    {
+        item: 'Shirt',
+        color: 'blue',
+        size: 'm',
+        qty: 10,
+        price: 20,
+        uuid: 'dd161ffe-d0f5-9545-8c4f-8462945323dd',
+        discount: 0,
+        img: '/Images/blue-demo-shirt.png',
+    },
+
+    {
+        item: 'Shirt',
+        color: 'blue',
+        size: 'l',
+        qty: 12,
+        price: 20,
+        uuid: 'gh161ffe-dff5-9545-8c4f-8462944763dd',
+        discount: 0,
+        img: '/Images/blue-demo-shirt.png',
+    },
+
+
+
+
+]

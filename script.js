a4a072674cb2b7741b27bd2769da0fceb6162c5c
JohnAlexR <128446488+JohnAlexR@users.noreply.github.com>
2023-06-07 14:50:38 -0400

    render inventory to feed

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 import { inventory } from '/data.js'
 
+const inventoryFeed = document.getElementById('inventory-feed')
+
+
+function renderInventory() {
+
+    let innerHtml = ''
+
+    inventory.forEach((product) => {
+
+        innerHtml += `
+        
+                <div class='inventory'>
+                    <div class='inventory-item'>
+                        <p>${product.item}</p>
+                    </div>
+                    <div class='inventory-price'>
+                        <p>$${product.price}</p>
+                    </div>
+                    <div class='inventory-qty'>
+                        <p>qty: ${product.qty}</p>
+                    </div>
+                </div>
+             `
+    })
+
+    inventoryFeed.innerHTML = innerHtml
+
+}
+
+renderInventory()
